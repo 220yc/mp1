56dc9972a4b5ce655137999f9c47a290824f59e2
YingCian Liou <yingcianliou@YingCiandeMacBook-Air-3.local>
2026-09-16 15:41:39 -0500

    Fix GitHub Pages asset paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,110 @@
 const path = require("path");
+
 const CopyPlugin = require("copy-webpack-plugin");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+
 module.exports = {
+
   mode: "development",
+
   devtool: "source-map",
+
   devServer: {
-    static: { directory: path.resolve(__dirname, 'build') },
+
+    static: {
+      directory: path.resolve(__dirname, "build")
+    },
+
     open: true,
+
     host: "localhost",
-    watchFiles: 'index.html',
+
+    watchFiles: "index.html",
+
   },
-  context: path.join(__dirname, 'src'),
+
+
+  context: path.join(__dirname, "src"),
+
   entry: "./index.js",
+
+
   module: {
+
     rules: [
+
       {
         test: /\.(js|jsx)$/i,
         loader: "babel-loader",
       },
+
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ],
       },
+
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
+
       {
         test: /\.html$/i,
         loader: "html-loader",
       },
+
     ],
+
   },
+
+
   plugins: [
+
     new CopyPlugin({
+
       patterns: [
-        { from: './assets/', to: './assets/' },
+
+        {
+          from: "./assets/",
+          to: "./assets/"
+        },
+
       ],
+
     }),
+
+
     new HtmlWebpackPlugin({
+
       template: "index.html",
-      inject: 'body',
+
+      inject: "body",
+
     }),
+
   ],
+
+
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, "build"),
+
+    filename: "bundle.js",
+
+    path: path.resolve(
+      __dirname,
+      "build"
+    ),
+
+    publicPath: "./",
+
+    clean: true,
+
   },
+
 };
